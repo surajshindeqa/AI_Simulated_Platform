7aff152412616cd6d14edc483c4e5afba38f99f5
Suraj Shinde <suraj@audirie.com>
2024-11-19 07:22:51 +0530

    Updated script

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,32 +22,22 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
+  //use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+    //trace: 'on-first-retry',
+  //},
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    }
 
     /* Test against mobile viewports. */
     // {
@@ -69,6 +59,12 @@ export default defineConfig({
     //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     // },
   ],
+  reporter: [['allure-playwright']],
+  use: {
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video:'on'
+  }
 
   /* Run your local dev server before starting the tests */
   // webServer: {
